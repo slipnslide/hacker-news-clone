b9ba07020944588901cd8169988071c456339644
Slip <email": null>
2017-05-08 17:07:36 -0400

    began writing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,20 @@
 class CommentsController < ApplicationController
 
 	def new
+		@comment = Comment.new
 	end
 
 	def create
+		@comment = Comment.new(comment_params)
+		@user = current_user
+
+		if @comment.save
+			format.html { redirect_to @comment, notice: 'Post was successfully created.' }
+			format.json { render :show, status: :created, location: @comment }
+		else
+			format.html { render :new }
+			format.json { render json: @comment.errors, status: :unprocessable_entity }
+		end
 	end
 
 	private
